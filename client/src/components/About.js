adab0ff8af3dbf4856bf33888714f8ee79cbeebb
ombasaabuya <ombasabuya@gmail.com>
2023-04-09 08:09:33 +0300

    first commit

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,11 +7,11 @@ function About() {
   <div className="flex flex-row">
   <div className="ml-40">
     <div className="mt-2">
-        <h1 className="text-[44px]">Automatic Pairing</h1>
+        <h1 className="text-[44px]">Soma App</h1>
     <img className="object-cover h-40 w-96" src="https://trello.com/1/cards/6352455bf4c39c009967a985/attachments/635288c74a99790283d6f2cc/previews/635288c84a99790283d6f2d8/download/aboutus.jpg" alt="about"/>
     </div>
     <div className="text-center w-60">
-    <p >“You go through life wondering what is it all about life at the end of the day”</p>
+    <p >“Welcome to Soma app”</p>
     </div>
     <div className="flex flex-row">
     
@@ -21,50 +21,20 @@ function About() {
     But what do we say?
     </div>
     <div className="bg-[#713f12] h-8 w-24 text-center">
-    Automation
+    Soma Automated
  </div>
  </div>
     </div>
     </div>
     
     <div className="bg-[#e5e5e5] w-80  text-center ml-80 mt-4 mb-4 ">
-    <p >We provide a  solution to the pairing  problem. For 
-        the teachers, leaders,<br></br>
-        lectures and other groups 
-          who are involved in dealing   with users 
-         or people,<br></br>
-          this  system is for 
-          you. <br></br>
-          It allows you to automatically put people 
-            into pairs and keep track 
-             of the pairs at just a click 
-             of a button. <br></br>
-This gives efficiency in 
-dealing with large number <br></br>
- of people.</p>
+    <p >SOMA is one of the most  digital transformation in Africa that has hugely impacted content creators, corporate organizations 
+    and government institutions with a consistent call to adopt digital technology in everyday operations.</p>
  </div>
  
  </div>
 {/* <div className="bg-[#1D6697] h-36"></div> */}
-<footer className="bg-[#1D6697] h-36 text-white">
-          <div className="flex">
-            <div className="pr-96 pl-4 pt-8">
-              <h3>Feel free to reach us out!</h3>
-            </div>
-            <div className="pr-96 pt-8">
-              <h3>Visit us Today!</h3>
-              <a href="#" class="fa fa-facebook"></a>
-              <a href="#" class="fa fa-twitter"></a>
-              <a href="#" class="fa fa-instagram"></a>
-              <h3>Copyright: AutomaticPairing</h3>
-            </div>
-            <div className="pt-8">
-              <h3>Get in touch</h3>
-              <h3>Email</h3>
-              <h4>02-200-000</h4>
-            </div>
-          </div>
-        </footer>
+
   </>
   )
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -23,71 +23,27 @@ function Home() {
         <div className="flex">
           <div className="p-36 text-2xl w-6/12">
             <h2 className="text-black bg-white border-[#1D6697] border-4 p-4 w-4/5 font-medium rounded-lg shadow-lg text-centre">
-              Putting students into random <br></br> pairings takes FOREVER.{" "}
-              <br></br> Automatic pairing app is here to <br></br> save you
-              hours each week! <br></br> Providing a fun, free, and easy way to{" "}
-              <br></br> create student study pairs. We provide a solution to the
-              pairing problem. For the teachers, leaders,<br></br>
-              lectures and other groups who are involved in dealing with users
-              or people,<br></br>
-              this system is for you. <br></br>
+              Putting students into learning pairs
               It allows you to automatically put people into pairs and keep
               track of the pairs at just a click of a button. <br></br>
               This gives efficiency in dealing with large number <br></br>
               of people.
             </h2>
           </div>
-          {/* <div className="w-6/12 h-full">
-            <img src="" alt="Italian Trulli"></img>
-          </div> */}
         </div>
 
-        {/* <div className="flex flex-row mt-1">
-          <div className="ml-40">
-            <div className="mt-2">
-              <img
-                className="object-cover h-40 w-96"
-                src="https://trello.com/1/cards/6352455bf4c39c009967a985/attachments/635288c74a99790283d6f2cc/previews/635288c84a99790283d6f2d8/download/aboutus.jpg"
-                alt="about"
-              />
-            </div>
-            <div className="text-center w-60">
-              <p>
-                “You go through life wondering what is it all about life at the
-                end of the day”
-              </p>
-            </div>
-            <div className="flex flex-row">
-              <img
-                className="rounded-full object-cover h-36 w-36"
-                src="https://trello.com/1/cards/6352455bf4c39c009967a985/attachments/635288e5c53ace0127a7f7d0/previews/635288e6c53ace0127a7f7d8/download/every.jpg"
-                alt="about"
-              />
-              <div className="ml-8 mt-4">
-                <div className="text-[#1D6697] text-[19]">
-                  But what do we say?
-                </div>
-                <div className="bg-[#713f12] h-8 w-24 text-center text-white">
-                  Automation
-                </div>
-              </div>
-            </div>
-          </div>
-
-          
-        </div> */}
 
         <div className="contact mt-20">
           <div className="background">
             <div
               style={{
-                backgroundImage: `url('https://trello.com/1/cards/6354e878c89b2702f9492e14/attachments/6354f42f2811e7024ebd8d67/previews/6354f4302811e7024ebd8d75/download/contact.png')`,
+                backgroundImage: `url('https://images.pexels.com/photos/5212350/pexels-photo-5212350.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
               }}
             >
               <div className="mx-40">
                 <h1 className="text-6xl pb-8 pt-4">Contact us </h1>
                 <h3 className="text-2xl pb-4">
-                  Our friendly team would like to hear from you
+                  would like to hear from you
                 </h3>
                 <div className="bg-[#EDF4F9] border-2 border-[#1D6697] rounded-3xl w-2/3 ">
                   <div className="p-8">
@@ -144,26 +100,6 @@ function Home() {
             </div>
           </div>
         </div>
-        <Footer />
-        {/* <footer className="bg-[#1D6697] h-36 text-white">
-          <div className="flex">
-            <div className="pr-96 pl-4 pt-8">
-              <h3>Feel free to reach us out!</h3>
-            </div>
-            <div className="pr-96 pt-8">
-              <h3>Visit us Today!</h3>
-              <a href="#" class="fa fa-facebook"></a>
-              <a href="#" class="fa fa-twitter"></a>
-              <a href="#" class="fa fa-instagram"></a>
-              <h3>Copyright: AutomaticPairing</h3>
-            </div>
-            <div className="pt-8">
-              <h3>Get in touch</h3>
-              <h3>Email</h3>
-              <h4>02-200-000</h4>
-            </div>
-          </div>
-        </footer> */}
       </div>
     </>
   );
